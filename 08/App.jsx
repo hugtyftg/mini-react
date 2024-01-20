9f977e60b3301369c99d6cc5415facf79f0a4442
mmy <15224924822@163.com>
2024-01-20 14:13:27 +0800

    实现多个useEffect监听多个deps

---
+++ 08/App.jsx
@@ -0,0 +1,38 @@
+import React from './core/React';
+let countFoo = 1;
+// useEffect调用时机是React渲染真实DOM之后，并且浏览器完成重新绘制之前
+function Foo() {
+  console.log('foo');
+  const [count, setCount] = React.useState(0);
+  const [bar, setBar] = React.useState('bar');
+  function handleClick() {
+    setCount((count) => count + 1);
+    setBar((bar) => bar + 'bar');
+    // setBar((bar) => 'bar');
+  }
+  React.useEffect(() => {
+    console.log('init');
+  }, []);
+  React.useEffect(() => {
+    console.log('update');
+  }, [count]);
+  return (
+    <div>
+      <h1>Foo</h1>
+      <div>{bar}</div>
+      {count}
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
+}
+
+// 自动识别jsx语法
+function App() {
+  return (
+    <div>
+      hi-mini-react count
+      <Foo />
+    </div>
+  );
+}
+export default App;
