5cb8b973f776709d2064486f31c27026791da414
mmy <15224924822@163.com>
2024-01-26 21:34:24 +0800

    实现函数组件

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,4 +1,20 @@
 import React from './core/React';
+type CountType = {
+  num: number;
+};
+function Count({ num }: CountType) {
+  return (
+    <div
+      className="count"
+      style={{
+        fontStyle: 'italic',
+        textAlign: 'center',
+      }}
+    >
+      count:{num}
+    </div>
+  );
+}
 const App = (
   <div id="app">
     <h1
@@ -6,11 +22,13 @@ const App = (
         color: 'white',
         backgroundColor: 'lightblue',
         textAlign: 'center',
+        fontSize: '50px',
       }}
     >
       mini react
     </h1>
-    <p style={{ textAlign: 'center' }}>typescript version</p>
+    <p style={{ textAlign: 'center', fontSize: '20px' }}>typescript version</p>
+    <Count num={10} />
   </div>
 );
 
